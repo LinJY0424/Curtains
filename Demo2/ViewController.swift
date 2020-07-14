2d530ee82186963c2d41adbd07cde25890742385
LinJY0424 <f0983344166@gmail.com>
2020-07-14 22:10:07 +0800

    Initial Commit

---
+++ Demo2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by Duc-Thang Vo on 2020/7/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
